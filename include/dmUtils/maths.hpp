3b3050992c8a7ef5287e05f6f28e5ff747f58e7d
DaiMysha <mlp.lemon.birdy@gmail.com>
2017-01-05 15:34:31 +0100

    Fixed fast sqrt to avoid warnings

--- include/dmUtils/maths.hpp
+++ include/dmUtils/maths.hpp
@@ -68,21 +68,25 @@ namespace maths {
 	template <int N>
 	struct fact;
 
-	/*
-	inline float sqrt(float number) {
+	//fast sqrt float cheat
+	inline float sqrt(float number) 
+	{
 		long i;
 		float x, y;
 		const float f = 1.5F;
 		x = number * 0.5F;
 		y = number;
-		i = * ( long * ) &y;
+		long* ip = reinterpret_cast<long*>(&y); //gets rid of strict-aliasing warnings
+		i = *ip;
 		i = 0x5f3759df - ( i >> 1 );
-		y = * ( float * ) &i;
+		float* yp = reinterpret_cast<float*>(&i);
+		y = *yp;
+		
+		y = y * ( f - ( x * y * y ) );
 		y = y * ( f - ( x * y * y ) );
 		y = y * ( f - ( x * y * y ) );
 		return number * y;
 	}
-	//*/
 
 }
 }
